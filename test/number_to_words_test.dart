d3af740d980c233de5df76f1ad7feb1ae0e336de
Benjith <mpbenjith@gmail.com>
2019-03-12 15:24:51 +0530

    initial commit

---
+++ test/number_to_words_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:number_to_words/number_to_words.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final numberToWord = NumberToWord();
+    expect(numberToWord.convert('en-in',2), 'two');
+    expect(numberToWord.convert('en-in',5), 'five');
+    expect(() => numberToWord.convert('sda',null), throwsNoSuchMethodError);
+  });
+}
